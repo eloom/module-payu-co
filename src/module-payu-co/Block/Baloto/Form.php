e33343f4c4eb8f48ddde0d82d55667c0a3cd215a
dholi <comunidade@dholi.dev>
2021-06-17 21:42:32 -0300

    Url elOOm

--- src/module-payu-co/Block/Baloto/Form.php
+++ src/module-payu-co/Block/Baloto/Form.php
@@ -5,7 +5,7 @@
 * 
 * @category     Ã©lOOm
 * @package      Modulo PayUCo
-* @copyright    Copyright (c) 2021 Ã©lOOm (https://www.eloom.dev)
+* @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
 * @version      1.0.0
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
