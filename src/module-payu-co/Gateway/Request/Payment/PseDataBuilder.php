febb4c81ecbd7073b8e327d33a91d61af31f94ed
dholi <comunidade@dholi.dev>
2021-11-04 05:46:37 -0300

    Update PseDataBuilder.php

--- src/module-payu-co/Gateway/Request/Payment/PseDataBuilder.php
+++ src/module-payu-co/Gateway/Request/Payment/PseDataBuilder.php
@@ -18,7 +18,9 @@ namespace Eloom\PayUCo\Gateway\Request\Payment;
 use Eloom\PayU\Gateway\PayU\Enumeration\Country;
 use Eloom\PayU\Gateway\PayU\Enumeration\PaymentMethod;
 use Eloom\PayU\Gateway\Request\Payment\AuthorizeDataBuilder;
+use Eloom\PayU\Helper\MappedOrderAttributeDefinition;
 use Eloom\PayUCo\Gateway\Config\Baloto\Config;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\HTTP\Header;
 use Magento\Framework\Stdlib\CookieManagerInterface;
 use Magento\Framework\UrlInterface;
@@ -26,23 +28,23 @@ use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 
 class PseDataBuilder implements BuilderInterface {
-	
+
 	const COOKIE = 'cookie';
-	
+
 	const USER_AGENT = 'userAgent';
-	
+
 	const PAYMENT_METHOD = 'paymentMethod';
-	
+
 	const EXPIRATION_DATE = 'expirationDate';
-	
+
 	private $config;
-	
+
 	private $cookieManager;
-	
+
 	private $httpHeader;
-	
+
 	protected $urlBuilder;
-	
+
 	public function __construct(Config                 $config,
 	                            CookieManagerInterface $cookieManager,
 	                            Header                 $httpHeader,
@@ -52,29 +54,30 @@ class PseDataBuilder implements BuilderInterface {
 		$this->httpHeader = $httpHeader;
 		$this->urlBuilder = $urlBuilder;
 	}
-	
+
 	public function build(array $buildSubject) {
 		$paymentDataObject = SubjectReader::readPayment($buildSubject);
 		$payment = $paymentDataObject->getPayment();
 		$order = $payment->getOrder();
-		
-		$taxvat = ($order->getCustomerTaxvat() ? $order->getCustomerTaxvat() : $order->getBillingAddress()->getVatId());
+
+		$attributeDefinition = ObjectManager::getInstance()->get(MappedOrderAttributeDefinition::class);
+		$taxvat = $attributeDefinition->getTaxvat($order);
 		$taxvat = preg_replace('/\D/', '', $taxvat);
-		
+
 		$extraParameters = [
 			'RESPONSE_URL' => $this->urlBuilder->getUrl('sales/order/history'),
 			'FINANCIAL_INSTITUTION_CODE' => $payment->getAdditionalInformation('pseBank'),
 			'USER_TYPE' => $payment->getAdditionalInformation('pseUserType'),
 			'PSE_REFERENCE3' => $taxvat,
 		];
-		
+
 		$country = Country::memberByKey($order->getOrderCurrencyCode());
 		if ($country->isColombia()) {
 			if (null != $order->getBillingAddress()->getDnitype()) {
 				$extraParameters['PSE_REFERENCE2'] = $order->getBillingAddress()->getDnitype();
 			}
 		}
-		
+
 		return [AuthorizeDataBuilder::TRANSACTION => [
 			self::PAYMENT_METHOD => PaymentMethod::memberByKey('pse')->getCode(),
 			self::COOKIE => $this->cookieManager->getCookie('PHPSESSID'),
