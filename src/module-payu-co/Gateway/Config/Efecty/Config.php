b8678d2657cd4f92e3a3804b910ad0d09297687a
dholi <comunidade@dholi.dev>
2022-05-17 16:36:33 -0300

    Magento 2.4.4 support

--- src/module-payu-co/Gateway/Config/Efecty/Config.php
+++ src/module-payu-co/Gateway/Config/Efecty/Config.php
@@ -5,7 +5,7 @@
 * 
 * @category     elOOm
 * @package      Modulo PayUCo
-* @copyright    Copyright (c) 2022 Ã©lOOm (https://eloom.tech)
+* @copyright    Copyright (c) 2022 elOOm (https://eloom.tech)
 * @version      2.0.0
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
